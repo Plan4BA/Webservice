c167a235e4f28a33dc51119e98a58b91312854c4
DDKFM <maxschaedlich@hotmail.de>
2018-10-20 12:50:24 +0200

    full implmented caldav-interface (lecture, meals, both)

--- src/main/kotlin/de/ddkfm/plan4ba/controller/CaldavController.kt
+++ src/main/kotlin/de/ddkfm/plan4ba/controller/CaldavController.kt
@@ -4,6 +4,7 @@ import biweekly.Biweekly
 import biweekly.ICalendar
 import biweekly.component.VEvent
 import biweekly.property.Image
+import biweekly.util.Duration
 import com.mashape.unirest.http.Unirest
 import de.ddkfm.plan4ba.models.*
 import de.ddkfm.plan4ba.utils.toModel
@@ -26,9 +27,9 @@ class CaldavController(req : Request, resp : Response, user : User) : Controller
     @GET
     @ApiOperation(value = "get caldav lectures")
     @ApiResponses(
-            ApiResponse(code = 200, message = "successfull", response = Meal::class, responseContainer = "List")
+            ApiResponse(code = 200, message = "successfull")
     )
-    @Path("")
+    @Path("/lectures")
     fun getCalDav(): Any? {
         try {
             val lectureController = LectureController(req, resp, user)
@@ -72,5 +73,68 @@ class CaldavController(req : Request, resp : Response, user : User) : Controller
             e.printStackTrace()
             return ""
         }
+    }
+
+    @GET
+    @ApiOperation(value = "get meals as caldav resource")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "successfull")
+    )
+    @Path("/meals")
+    fun getMeals(): Any? {
+        try {
+            val mealController = MealController(req, resp, user)
+            val meals = mealController.getMeals() as List<Meal>
+            val calendar = ICalendar()
+            val group = Unirest
+                    .get("${config.dbServiceEndpoint}/groups/${user.groupId}")
+                    .toModel(UserGroup::class.java)
+                    .second as UserGroup
+            val university = Unirest.get("${config.dbServiceEndpoint}/universities/${group.universityId}")
+                    .toModel(University::class.java)
+                    .second as University
+
+            val geoLocation = Unirest.get("${config.dbServiceEndpoint}/universities/${university.id}/location")
+                    .asJson().body.`object`.toModel(Geo::class.java)
+
+            val events = meals.map { meal ->
+                val dailyEvents = meal.meals.map { food ->
+                    val event = VEvent()
+                    val summary = event.setSummary(food.description)
+                    summary.language = "de-DE"
+                    event.setDateStart(Date(meal.day))
+                    event.setDuration(Duration.fromMillis(1800 * 1000))
+                    event.setLocation(university.name)
+                    val description = "${food.description}\n ${food.prices} \n ${food.additionalInformation}"
+                    event.setDescription(description)
+
+                    event.geo = biweekly.property.Geo(geoLocation.latitude, geoLocation.longitude)
+                    event
+                }
+                dailyEvents
+            }.flatMap{ it }
+            return Biweekly.write(calendar).go()
+        } catch (e : Exception) {
+            e.printStackTrace()
+            return ""
+        }
+    }
+
+    @GET
+    @ApiOperation(value = "get lectures and meals as caldav resource")
+    @ApiResponses(
+            ApiResponse(code = 200, message = "successfull")
+    )
+    @Path("/all")
+    fun getAll(): Any? {
+        val caldavController = CaldavController(req, resp, user)
+        val mealsCalStr = caldavController.getMeals() as String
+        val lectureCalStr = caldavController.getCalDav() as String
+
+        val lectureCal = Biweekly.parse(mealsCalStr).first()
+        val mealsCal = Biweekly.parse(lectureCalStr).first()
+        val globalCal = ICalendar(lectureCal)
+        globalCal.events.addAll(mealsCal.events)
+        return Biweekly.write(globalCal).go()
     }
 }
