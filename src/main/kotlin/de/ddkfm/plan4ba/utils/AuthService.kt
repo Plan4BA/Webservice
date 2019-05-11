c32ebeacd36dea60282cfd783c2cd86059c45b1d
DDKFM <maxschaedlich@hotmail.de>
2019-05-11 16:09:28 +0200

    update user with new boolean flags

--- src/main/kotlin/de/ddkfm/plan4ba/utils/AuthService.kt
+++ src/main/kotlin/de/ddkfm/plan4ba/utils/AuthService.kt
@@ -4,15 +4,31 @@ import kong.unirest.Unirest
 import de.ddkfm.plan4ba.models.*
 
 object AuthService {
-    fun modifyHash(u : User, storeHash : Boolean, password: String) : User {
-        var user = u
-        if(user.userHash.isNullOrEmpty() xor storeHash)
-            return user
+    fun modifyHash(u : User, storeHash : Boolean, storeReminders : Boolean, storeExamStats : Boolean, password: String) : User {
+        var user = u.copy()
+
         if(!storeHash)
             user.userHash = ""
-        else
+        else if(storeHash && user.userHash.isNullOrEmpty()) {
             user = loginCampusDual(user.matriculationNumber, password)!!
+        }
+        if(user.storeExamsStats && !storeExamStats) {
+            val examStats = DBService.all<ExamStat>("userId" to user.id).maybe ?: emptyList()
+            for(examStat in examStats)
+                DBService.delete<ExamStat>(examStat.id)
+        }
+        if(user.storeReminders && !storeReminders) {
+            val reminders = DBService.all<Reminder>("userId" to user.id).maybe ?: emptyList()
+            for(reminder in reminders) {
+                DBService.delete<Reminder>(reminder.id)
+            }
+        }
+        user.storeExamsStats = storeExamStats
+        user.storeReminders = storeReminders
         val respUser = DBService.update(user) { it.id }.getOrThrow()
+        if(!u.storeExamsStats && storeExamStats || !u.storeReminders && storeReminders) {
+            triggerCaching(respUser, respUser.userHash ?: "")
+        }
         return respUser
     }
     fun loginCampusDual(username : String, password : String) : User? {
@@ -73,6 +89,7 @@ object AuthService {
 
 
     fun triggerCaching(user : User, hash : String) {
+        println("trigger caching for user ${user.id}")
         val job = LectureJob(user.id, hash, user.matriculationNumber)
         Unirest.post("${de.ddkfm.plan4ba.config.cachingServiceEndpoint}/trigger")
             .body(job.toJson())
