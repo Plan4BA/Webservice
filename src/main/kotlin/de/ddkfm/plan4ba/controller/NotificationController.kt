36c100f2b5cf196b21f008f490bff360b64a3b43
DDKFM <maxschaedlich@hotmail.de>
2019-05-11 16:06:45 +0200

    implement new notification structure

--- src/main/kotlin/de/ddkfm/plan4ba/controller/NotificationController.kt
+++ src/main/kotlin/de/ddkfm/plan4ba/controller/NotificationController.kt
@@ -14,15 +14,38 @@ import kotlin.contracts.ExperimentalContracts
 @Produces("application/json")
 @ShortToken
 class NotificationController(req : Request, resp : Response, user : User) : ControllerInterface(req = req, resp = resp, user = user) {
-    @ExperimentalContracts
+
     @GET
     @Path("")
-    fun getNotifications(): List<SimpleNotification> {
+    fun getNotifications(@QueryParam("language") language : String): List<SimpleNotification> {
+        val language = if(language.isEmpty()) "de" else language
         val notificationsResponse = DBService.all<Notification>("userId" to user.id)
         if(notificationsResponse.error != null)
             throw notificationsResponse.error.asException()
-        val notifications = notificationsResponse.maybe
-            ?.map { SimpleNotification(it.id, it.label, it.description, it.type) }
+        val notifications = notificationsResponse.maybe?.map { notification ->
+            val translation = DBService.all<NotificationTranslation>("type" to notification.type, "language" to language).maybe?.firstOrNull()
+                ?: NotificationTranslation(0, notification.type, "de", "nicht näher benannte Benachrichtigung", "Ich bin eine Notification")
+            val callback = when(notification.type) {
+                NotificationType.LECTURE_CHANGE.value -> {
+                    "/changes/${notification.id}"
+                }
+                NotificationType.APP_CHANGE.value -> {
+                    val versionId = notification.versionId
+                    if(versionId == null)
+                        System.err.println("VersionID of notification ${notification.id} is null, but type is appChange")
+                    "/versions/$versionId"
+                }
+                else -> throw InternalServerError("type ${notification.type} is not supported").asException()
+            }
+            val simpleNotification = SimpleNotification(
+                id = notification.id,
+                label = translation.label,
+                description = translation.description,
+                type = notification.type,
+                callback = callback
+            )
+            simpleNotification
+        }
         return notifications ?: emptyList()
     }
 
