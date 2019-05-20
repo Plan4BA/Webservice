c9a131e01b61c79c16f3740adf1b3f9dad492aba
DDKFM <maxschaedlich@hotmail.de>
2019-05-20 17:11:51 +0200

    remove workaround where the CALDAV-Link is merged with group and university links

--- src/main/kotlin/de/ddkfm/plan4ba/controller/LinksController.kt
+++ src/main/kotlin/de/ddkfm/plan4ba/controller/LinksController.kt
@@ -23,13 +23,11 @@ class LinksController(req : Request, resp : Response, user : User) : ControllerI
     @Path("")
     fun getUniversityLinks(@QueryParam("language") language : String): List<SimpleLink> {
         val actualLanguage = if(language.isEmpty()) "de" else language
-        val caldavToken = UserController(req, resp,user).getCaldavToken()
         val userLinks = (Unirest.get("${config.dbServiceEndpoint}/users/${user.id}/links")
             .toModel(Link::class.java)
             .second as List<Link>)
             .map { SimpleLink(it.id, it.label, it.url, it.language) }
             .filter { actualLanguage == it.language }
-        val caldavLink = SimpleLink(0, "CALDAV-Link", "${req.scheme()}://${req.host()}/api/caldav/all?token=${caldavToken.token}", actualLanguage)
-        return userLinks.union(listOf(caldavLink)).toList()
+        return userLinks
     }
 }
